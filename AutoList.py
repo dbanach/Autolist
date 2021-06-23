03b3ff3958ca5375cf1dfbfe6e3c240ac364b51e
i-kahan <ikahan4@gmail.com>
2021-06-23 15:59:09 -0300

    AutoList_update

--- AutoList.py
+++ AutoList.py
@@ -1,7 +1,9 @@
 from selenium import webdriver
-from selenium.webdriver.common.action_chains import ActionChains
+import re
+from selenium.common.exceptions import NoSuchElementException
+import car_ad_data
 
-URL = 'https://www.autolist.com/listings#page=1&latitude=32.0668&location=Tel+Aviv%2C+TA&longitude=34.7649&radius=any'
+URL = 'https://www.autolist.com/listings#page=1&latitude=40.7127753&location=New+York%2C+NY&longitude=-74.0059728&body_style%5B%5D=convertible'
 
 
 # def get_all_links(driver):
@@ -15,7 +17,7 @@ URL = 'https://www.autolist.com/listings#page=1&latitude=32.0668&location=Tel+Av
 def get_all_car_links_in_page(driver):
     """
     returns a list of all the links to the specifics adds showed in one page of Autolist
-    :return:
+    :return: list of individual car ads
     """
     return driver.find_elements_by_class_name(name='vehicle-item-view')
 
@@ -23,14 +25,15 @@ def get_all_car_links_in_page(driver):
 def loops_through_adds(driver):
     """
     function that receives a driver and loops through a list of webpages
-    :param driver:
-    :return:
+    :param driver: selenium web driver
+    :return: None
     """
-
+    driver.implicitly_wait(5)
     for one_add in get_all_car_links_in_page(driver):
-
-        pass
-
+        this_add = one_add.find_element_by_tag_name(name='a')
+        href = this_add.get_attribute("href")
+        soup = car_ad_data.get_data(href)
+        car_ad_data.get_car_info(soup)
     print('one loop')
 
 
@@ -38,19 +41,20 @@ def loops_through_adds(driver):
 # might need to adapt and receive something that saves the info
 
 
-def next_page(driver):
+def next_page(driver, page):
     """
     function that checks if there is a next page.
     if there is it clicks on it and returns true, if not does returns false
 
     """
+    new_page = int(re.search(r'page=(\w*)', URL).group(1)) + page
+    new_URL = re.sub(r'page=(\w*)', 'page=' + str(new_page), URL)
 
-    possible_next = driver.find_element_by_id(id_='pagination-region').find_element_by_tag_name(
-        name='div').find_element_by_tag_name(name='ul').find_elements_by_tag_name(name='li')[-1]
+    driver.get(new_URL)
 
-    if possible_next.text == 'Next':
-        webdriver.ActionChains(driver).move_to_element(possible_next).click(possible_next).perform()
-        # possible_next.find_element_by_tag_name(name='a').click()
+    try:
+        no_results = driver.find_element_by_xpath("//h3[text()='No Results Found']")
+    except NoSuchElementException:
         return True
     else:
         return False
@@ -62,13 +66,18 @@ def main():
     driver = webdriver.Chrome()
     driver.get(URL)
 
+    loops = 1
     while True:
+
         loops_through_adds(driver)
 
-        val = next_page(driver)
+        val = next_page(driver, loops)
 
         if not val:
+            driver.close()
             break
+
+        loops += 1
 
 
 if __name__ == '__main__':
