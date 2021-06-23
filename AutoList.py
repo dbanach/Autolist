fad4d48389daba0fff01eb4beb33d71c80e208aa
Daniel Banach <dbanach.tech@gmail.com>
2021-06-23 07:17:23 -0400

    Autolist.py and readme update

---
+++ AutoList.py
@@ -0,0 +1,75 @@
+from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+
+URL = 'https://www.autolist.com/listings#page=1&latitude=32.0668&location=Tel+Aviv%2C+TA&longitude=34.7649&radius=any'
+
+
+# def get_all_links(driver):
+#   links = []
+#  elements = driver.find_elements_by_tag_name('a')
+# for elem in elements:
+#    href = elem.get_attribute("href")
+#   links.append(href)
+# return links
+
+def get_all_car_links_in_page(driver):
+    """
+    returns a list of all the links to the specifics adds showed in one page of Autolist
+    :return:
+    """
+    return driver.find_elements_by_class_name(name='vehicle-item-view')
+
+
+def loops_through_adds(driver):
+    """
+    function that receives a driver and loops through a list of webpages
+    :param driver:
+    :return:
+    """
+
+    for one_add in get_all_car_links_in_page(driver):
+
+        pass
+
+    print('one loop')
+
+
+# Here goes code of Ilan taking the info
+# might need to adapt and receive something that saves the info
+
+
+def next_page(driver):
+    """
+    function that checks if there is a next page.
+    if there is it clicks on it and returns true, if not does returns false
+
+    """
+
+    possible_next = driver.find_element_by_id(id_='pagination-region').find_element_by_tag_name(
+        name='div').find_element_by_tag_name(name='ul').find_elements_by_tag_name(name='li')[-1]
+
+    if possible_next.text == 'Next':
+        webdriver.ActionChains(driver).move_to_element(possible_next).click(possible_next).perform()
+        # possible_next.find_element_by_tag_name(name='a').click()
+        return True
+    else:
+        return False
+
+
+def main():
+    """ function that runs the program"""
+    global driver
+    driver = webdriver.Chrome()
+    driver.get(URL)
+
+    while True:
+        loops_through_adds(driver)
+
+        val = next_page(driver)
+
+        if not val:
+            break
+
+
+if __name__ == '__main__':
+    main()
--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # Autolist
 Scraping project using info from autolist
+
+The website being used for this project is www.Autolist.com
+It's a website with information about used cars on sale.
+
+We choose as a starting point a search that included all of the makes of
+cars, not minding how far it is to the location of the user.
+
+Then we located the parts on the webpage that would allow us to go through
+it scrapping.
+
+We located the html elements of all adds, getting the webpage for it.
+We then located the html element to go to the next page.
+In every add page we located all of the useful information to save.
+
+After that we wrote functions to allow us to do all of those parts.
+	1 - To loop through all the the adds.
+	2 - To click on them and get the information
+	3 - To go to the next page.
+	4 - To repeat the process until there's no more pages.
+
