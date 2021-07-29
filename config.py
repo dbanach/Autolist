d5a2ee7d896c535a1822eb1c947c29c2a5e5f592
Daniel Banach <dbanach.tech@gmail.com>
2021-07-29 23:44:44 +0300

    changes readme, config and requirements

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 DATABASENAME = 'CARS'
-
 DB_IP = "127.0.0.1"
 DB_USER = "root"
 DB_PASS = "project"
 CHARSET = "utf8mb4"
+API_URL = "https://car-data.p.rapidapi.com/cars"
+IMPLICIT_WAIT_TIME = 30
+NUMBER_OF_ADS_PER_PAGE = 20
